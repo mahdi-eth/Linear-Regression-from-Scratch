c71b162529e7708795bb1988fb73b6cf529f8082
haitham elmekaoui <96744352+ElmekaouiHaitham@users.noreply.github.com>
2025-03-17 22:11:14 +0000

    added l1 and l2 regularizations and test them in test_model

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 class LinearRegression:
-    def __init__(self, lr=0.001, n_iters=1000):
+    def __init__(self, lr=0.001, n_iters=1000, penalty = None):
         self.lr = lr
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
+        self.penalty = penalty
     
     def fit(self, X, y):
         n_samples, n_features = X.shape
@@ -17,6 +18,11 @@ class LinearRegression:
         for i in range(self.n_iters):
             y_pred = np.dot(X, self.weights) + self.bias
             dw = (1/n_samples) * (np.dot(X.T, (y_pred - y)))
+            # add regularization derevitive terme
+            if self.penalty == 'l1':
+                dw += self.lr * np.sign(self.weights)
+            elif self.penalty == 'l2':
+                dw += self.lr * 2 * self.weights
             db = (1/n_samples) * (np.sum(y_pred - y))
             self.weights = self.weights - (self.lr * dw)
             self.bias = self.bias - (self.lr * db)
